a3f160a1003cf62934202c635aa3ffd76f14b323
Sudar <sudar@sudarmuthu.com>
2010-01-25 07:21:18 +0000

    Changed link in footer

--- footer.php
+++ footer.php
@@ -10,8 +10,8 @@
 <?php endif; ?>
 
 <div id="footer">
-  <p> Copyright &copy;&nbsp;<?php echo date('Y');?> <?php bloginfo('name'); ?></p>
-  <p>Powered by WordPress and <a href="http://sudarmuthu.com/wordpress/bright-light" title="Bright Light Theme" target="_blank"><?php _e('Bright Light Theme', 'bright-light');?></a></p>
+  <p> <?php _e('Copyright','bright-light');?> &copy;&nbsp;<?php echo date('Y');?> <?php bloginfo('name'); ?></p>
+  <p><?php _e('Powered by WordPress and','bright-light');?> <a href="http://sudarmuthu.com/wordpress/bright-light" title="Bright Light Theme" target="_blank"><?php _e('Bright Light Theme', 'bright-light');?></a></p>
 <?php wp_footer(); ?>
 </div>
 </body>
