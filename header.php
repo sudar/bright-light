e316a9bf13d8119ab098b26217c35515c79f1600
Sudar <sudar@sudarmuthu.com>
2014-02-03 11:31:33 +0000

    Set viewport zoom level to 1

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 <head> 
 
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="initial-scale=1">
 <title><?php
 	/*
 	 * Print the <title> tag based on what is being viewed.
